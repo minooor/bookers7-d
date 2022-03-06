dbef88201d701204a8d65bfa6b0247c1953bb816
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-06 08:09:42 +0000

    [Add] Raty

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -3,5 +3,7 @@ class BookComment < ApplicationRecord
   belongs_to :book
 
   validates :comment, presence: true
+  # 星の評価の空を禁止する、且つ1以上、5未満
+  validates :rate, numericality: {　less_than_or_equal_to: 5,greater_than_or_equal_to: 1,}, presence: true
 
 end
